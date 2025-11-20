b3313e4d991681c6b6c9f9defb9de1f5a7a2b3c0
Gabriel Borba <xeroxsulensino@hotmail.com>
2025-11-20 17:35:04 -0300

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_URL || "/",
+  base: "/fittimer-pro/",
   server: {
     host: "::",
     port: 8080,
